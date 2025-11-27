a8f8919afc46cdf391cc187b117e8cdb504529ce
Jashanpreet Singh <jashan.irl@gmail.com>
2025-11-27 22:01:45 +0100

    Create MainTest.java

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+   
+    public void testAdd() {
+        assertEquals(5, Main.add(2, 3));
+    }
+}
